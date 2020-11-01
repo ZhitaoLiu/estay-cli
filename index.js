acb4612b6e9d24f482266496ccb0d8a7d89db4a1
lalala <lalala@appledeMacBook-Air.local>
2020-11-02 00:12:28 +0800

    [Add]：脚手架命令

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
 
-const { program } = require('commander')
+const { program } = require('commander');
 
 const package = require('./package.json');
+const helpOptions = require('./lib/core/help');
+const createCommands = require('./lib/core/create');
 
 // 脚手架版本
 program.version(package.version);
+
+// 帮助手册
+helpOptions();
+
+// 创建命令
+createCommands();
 
 // 解析
 program.parse(process.argv);
