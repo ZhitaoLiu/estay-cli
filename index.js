b043a133bf6effa57ad263323a6cbcc9b353070e
刘志涛 <damon.liu@estay.com>
2020-12-15 23:51:57 +0800

    feat(core): flutter代码自动生成

--- index.js
+++ index.js
@@ -2,12 +2,8 @@
 
 const { program } = require('commander');
 
-const package = require('./package.json');
 const helpOptions = require('./lib/core/help');
 const createCommands = require('./lib/core/create');
-
-// 脚手架版本
-program.version(package.version);
 
 // 帮助手册
 helpOptions();
@@ -15,5 +11,5 @@ helpOptions();
 // 创建命令
 createCommands();
 
-// 解析
+// 解析终端指令
 program.parse(process.argv);
